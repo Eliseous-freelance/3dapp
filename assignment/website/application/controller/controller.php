59bcd9e3b0166021958f0abcca571289c140afb4
Eliseous-freelance <96338130+Eliseous-freelance@users.noreply.github.com>
2022-04-17 11:21:10 +0100

    just got debug

--- assignment/website/application/controller/controller.php
+++ assignment/website/application/controller/controller.php
@@ -14,37 +14,96 @@ class Controller
     {
         //Create new Load and Model objects
         $this->load = new Load();
-        $this->model = new Model('sqlite:/its/home/ed385/public_html/3dapp/assignment/website/application/db/data.db');
 
         if ($pageID == 'home'){
+            $this->model = new Model('sqlite:/its/home/ed385/public_html/3dapp/assignment/website/application/db/infoData.db');
+            $this->model->dbCreateDrinkDetailsTable();
 
+            $fantaText = `We're all born playful creatures. But the older we become, the less permission we give
+                      ourselves to be playful and we can easily feel burnt out or overwhelmed. All this seriousness, we call it
+                      the "Grey".
+                      But we can always fight back the "Grey". How? By taking inspiration from the ones who stayed spontaneous
+                      and playful. The ones who bring colour to the world every day. The Colourful People.
+                      With our new campaign, Fanta wants to celebrate these Colourful People and reignite that spirit into
+                      people from all ages. So grab a Fanta and come play colourful with us.`;
+            $pepperText = `Dr Pepper's unique, sparkling blend of 23 fruit flavours has been around for well over
+              a century amd it's still the same, with that distinctive flavour you just can't quite put your toungue on.
+              It was created by Texas pharmacist Charles Alderton in 1885. Hw gave a sample of the first ever batch to
+              Wade Morrison, a local shop owner, and Mr Morrison instantly agreed to stock the drink.
+                The distinctive, bold taste of Dr Pepper has been popular ever since.`;
+            $cokeText = `In 1886 in New York Harbour, workers were constructing the Statue of Liberty. Eight
+              hundred miles away, another great American symbol was about to be unveiled. Like many people who change
+              history, John Stith Pemberton, an Atlanta pharmacist, was inspired by simple curiosity. One afternoon, he
+              stirred up a fragrant, caramel-coloured liquid and, when it was done ... booyah! He had invented Coca
+              Cola.`;
+            $spriteText = `First introduced in 1961, crisp, refreshing, clean-tasting Sprite is now the world's
+              leading lemon and lime flavoured soft drink and is sold in more than 190 different countries. Sprite Zero,
+              part of Coca Cola's no sugar Zero range, offers the delicious lemon lime taste of Sprite without the sugar
+              or calories.`;
+            $pepperPath = `/its/home/ed385/public_html/3dapp/assignment/website/assets/images/homepage/dr_pepper.jpg`;
+            $fantaPath = `/its/home/ed385/public_html/3dapp/assignment/website/assets/images/homepage/fanta.png`;
+            $cokePath = `/its/home/ed385/public_html/3dapp/assignment/website/assets/images/homepage/coca_cola.jpg`;
+            $spritePath = `/its/home/ed385/public_html/3dapp/assignment/website/assets/images/homepage/sprite.jpg`;
+            $this->model->dbInsertDrinkDetails($Id=3, $title='Dr Pepper', $text=$pepperText, $pepperPath);
+            $this->model->dbInsertDrinkDetails($Id=1, $title='Coca Cola', $text=$cokeText, $cokePath);
+            $this->model->dbInsertDrinkDetails($Id=2, $title='Sprite', $text=$spriteText, $spritePath);
+            $this->model->dbInsertDrinkDetails($Id=4, $title='Fanta', $text=$fantaText, $fantaPath);
+            $pepperDescriptionData = $this->apiGetInfo($model_function=$this->model->dbGetDrinkDetails());
+            $fantaDescriptionData = $this->apiGetInfo($model_function=$this->model->dbGetDrinkDetails());
+            $cokeDescriptionData = $this->apiGetInfo($model_function=$this->model->dbGetDrinkDetails());
+            $spriteDescriptionData = $this->apiGetInfo($model_function=$this->model->dbGetDrinkDetails());
+            $this->load->view($fileName=$fantaPath, $fantaDescriptionData);
+            $this->load->view($fileName=$cokePath, $cokeDescriptionData);
+            $this->load->view($fileName=$spritePath, $spriteDescriptionData);
+            $this->load->view($fileName=$pepperPath, $pepperDescriptionData);
         }
         else if ($pageID == 'coke'){
-
+            $cokePath = `/its/home/ed385/public_html/3dapp/assignment/website/assets/images/homepage/coca_cola.jpg`;
+            $this->model = new Model('sqlite:/its/home/ed385/public_html/3dapp/assignment/website/application/db/modelData.db');
+            $this->model->dbCreateModelTable();
+            $this->model->dbInsertModelData($ID=1, $x3dCreationMethod='Blender', $modelTitle='Coca Cola', $modelSubtitle='', $modelDescription='$pepperModelDescription');
+            $pepperModelData = $this->apiGetInfo($model_function=$this->model->dbGetModelData());
+            $this->load->view($fileName=$cokePath, $data=$pepperModelData);
         }
         else if ($pageID == 'sprite'){
-
+            $spritePath = `/its/home/ed385/public_html/3dapp/assignment/website/assets/images/homepage/sprite.jpg`;
+            $this->model = new Model('sqlite:/its/home/ed385/public_html/3dapp/assignment/website/application/db/modelData.db');
+            $this->model->dbCreateModelTable();
+            $this->model->dbInsertModelData($ID=2, $x3dCreationMethod='Blender', $modelTitle='Sprite', $modelSubtitle='', $modelDescription='$pepperModelDescription');
+            $pepperModelData = $this->apiGetInfo($model_function=$this->model->dbGetModelData());
+            $this->load->view($fileName=$spritePath, $data=$pepperModelData);
         }
         else if ($pageID == 'fanta'){
-
+            $fantaPath = `/its/home/ed385/public_html/3dapp/assignment/website/assets/images/homepage/fanta.png`;
+            $this->model = new Model('sqlite:/its/home/ed385/public_html/3dapp/assignment/website/application/db/modelData.db');
+            $this->model->dbCreateModelTable();
+            $this->model->dbInsertModelData($ID=4, $x3dCreationMethod='Blender', $modelTitle='Fanta', $modelSubtitle='', $modelDescription='$pepperModelDescription');
+            $pepperModelData = $this->apiGetInfo($model_function=$this->model->dbGetModelData());
+            $this->load->view($fileName=$fantaPath, $data=$pepperModelData);
         }
         else if ($pageID == 'pepper'){
-
+            $pepperPath = `/its/home/ed385/public_html/3dapp/assignment/website/assets/images/homepage/dr_pepper.jpg`;
+            $this->model = new Model('sqlite:/its/home/ed385/public_html/3dapp/assignment/website/application/db/modelData.db');
+            $this->model->dbCreateModelTable();
+            $this->model->dbInsertModelData($ID=3, $x3dCreationMethod='Blender', $modelTitle='Pepper', $modelSubtitle='', $modelDescription='$pepperModelDescription');
+            $pepperModelData = $this->apiGetInfo($model_function=$this->model->dbGetModelData());
+            $this->load->view($fileName=$pepperPath, $data=$pepperModelData);
         }
         else{
             echo "error in controller constructor. Wrong call id";
         }
     }
-    /**
-     * 
-     */
-    function controlPage($model_function, $fileName)
-    {
-        //get the brand data from the (this) Model using the dbGetbrand() method in this Model class
-        //get data from the defined model method
-        $data = $this->model->$model_function; //model3D_info(), dbCreateTable(), dbInsertData(), dbGetData(), dbGetBrand()
 
-        //tell the loader what view to load and which dat to use
-        $this->load->view($fileName, $data);
+    /**
+     * @param $model_function
+     * @return mixed
+     */
+    function apiGetInfo($model_function)
+    {
+        //get data from the defined model method
+        $data = $model_function;
+        return $data;
     }
+
+
 }
--- assignment/website/application/model/model.php
+++ assignment/website/application/model/model.php
@@ -33,34 +33,66 @@ class Model
         }
     }
 
-    public function dbCreateTable()
+    public function dbCreateModelTable()
     {
         try {
             $this->handle->exec("
                     CREATE TABLE [IF NOT EXISTS] Model_3D (
                         Id INTEGER PRIMARY KEY, 
-                        x3dModelTitle TEXT NOT NULL, 
                         x3dCreationMethod TEXT NOT NULL, 
                         modelTitle TEXT NOT NULL, 
                         modelSubtitle TEXT NOT NULL, 
                         modelDescription TEXT NOT NULL)");
-            return "Model_3D table is successfully created inside data.db file";
+            return "Model_3D table is successfully created inside infoData.db file";
         } catch (PDOException $e) {
             print new Exception($e->getMessage());
         }
         $this->handle = NULL;
     }
 
-    public function dbInsertData($x3dModelTitle, $x3dCreationMethod, $modelTitle, $modelSubtitle, $modelDescription)
+    public function dbCreateDrinkDetailsTable()
+    {
+        try {
+            $this->handle->exec("
+                    CREATE TABLE [IF NOT EXISTS] DrinkDetails (
+                        Id INTEGER PRIMARY KEY, 
+                        title TEXT, 
+                        text TEXT, 
+                        imagePath TEXT)");
+            return "drink details table is successfully created inside infoData.db file";
+        } catch (PDOException $e) {
+            print new Exception($e->getMessage());
+        }
+        $this->handle = NULL;
+    }
+
+    public function dbInsertModelData($ID, $x3dCreationMethod, $modelTitle, $modelSubtitle, $modelDescription)
     {
         echo "Data insertion function";
         try {
             $this->handle->exec("INSERT INTO Model_3D (Id, x3dModelTitle, x3dCreationMethod, modelTitle, modelSubstitle, modelSubtitle)
-                                    VALUES (1, 'string_1', 'string_2', 'string_3', 'string_4', 'string_5');" .
-                "INSERT INTO Model_3D (Id, x3dModelTitle, x3dCreationMethod, modelTitle, modelSubstitle, modelSubtitle)
-                                    VALUES (2, 'string_1', 'string_2', 'string_3', 'string_4', 'string_5');" .
-                "INSERT INTO Model_3D (Id, x3dModelTitle, x3dCreationMethod, modelTitle, modelSubstitle, modelSubtitle)
-                                    VALUES (3, 'string_1', 'string_2', 'string_3', 'string_4', 'string_5');");
+                                    SELECT $x3dCreationMethod, $modelTitle, $modelSubtitle, $modelDescription 
+                                    VALUES ($ID, $x3dCreationMethod, $modelTitle, $modelSubtitle, $modelDescription);
+                                    WHERE NOT EXISTS(SELECT $ID FROM Model_3D WHERE Id = $ID AND x3dCreationMethod = $x3dCreationMethod AND modelTitle = $modelTitle AND modelSubtitle = $modelSubtitle AND modelDescription = $modelSubtitle);");
+
+
+
+            return "X3D model data is inserted successfully inside modelData.db";
+        } catch (PDOException $e) {
+            print new Exception($e->getMessage());
+        }
+        $this->handle = NULL;
+    }
+
+    public function dbInsertDrinkDetails($Id, $title, $text, $imagePath)
+    {
+        echo "Data insertion function";
+        try {
+            $this->handle->exec(`INSERT INTO DrinkDetails (Id, title, text, imagePath) VALUES('$Id', '$title', '$text', '$imagePath')`);
+//            $this->handle->exec("INSERT INTO drinkDetails (Id, title, text, imagePath)
+//                                    SELECT $title, $text, $imagePath
+//                                    VALUES ($Id, $title, $text, $imagePath)
+//                                    WHERE NOT EXISTS(SELECT $Id FROM drinkDetails WHERE Id = $Id AND title = $title AND text = $text AND imagePath = $imagePath);");
             return "X3D model data is inserted successfully inside test1.db";
         } catch (PDOException $e) {
             print new Exception($e->getMessage());
@@ -68,7 +100,7 @@ class Model
         $this->handle = NULL;
     }
     
-    public function dbGetData()
+    public function dbGetModelData()
     {
         echo "Data retrieval function";
         try {
@@ -86,9 +118,6 @@ class Model
             //use PDO fetch() to retrieve the results from the database using a while loop
             //use a while loop to loop through the rows
             while ($data = $stmt->fetch()) {
-                //write the database contents to the results array for sending back the view
-                $result[$i]['brand'] = $data['brand'];
-                $result[$i]['x3dModelTitle'] = $data['x3dModelTitle'];
                 $result[$i]['x3dModelData'] = $data['x3dModelData'];
                 $result[$i]['modelTitle'] = $data['modelTitle'];
                 $result[$i]['modelSubtitle'] = $data['modelSubtitle'];
@@ -105,8 +134,40 @@ class Model
         //send the response back to the view
         return $result;
     }
-    public function dbDrinkDetails()
-    {
-        //
+
+    public function dbGetDrinkDetails(){
+        {
+            echo "Data retrieval function";
+            try {
+                //prepare a statement to get all records from the drinkDetails table
+                $sql = 'SELECT * FROM drinkDetails';
+                //use PDO query() to query the database with the prepared SQL statement
+                $stmt = $this->handle->query($sql);
+
+                //prepare a SQL statement to select a record matching the brand name selected in the view drop-down list
+
+                //user up an array to return the results to the view
+                $result = null;
+                //set up a variable to index each row of the array
+                $i = -0;
+                //use PDO fetch() to retrieve the results from the database using a while loop
+                //use a while loop to loop through the rows
+                while ($data = $stmt->fetch()) {
+                    $result[$i]['title'] = $data['title'];
+                    $result[$i]['text'] = $data['text'];
+                    $result[$i]['imagePath'] = $data['imagePath'];
+                    //increment the row index
+                    $i++;
+                }
+            } catch (PDOException $e) {
+                print new Exception($e->getMessage());
+                echo json_encode($result);
+            }
+            //close the database connection
+            $this->handle = NULL;
+            //send the response back to the view
+            return $result;
+        }
     }
+
 }
