1cfe02b39d60c35a5562b3cf0956596658e0f029
Eliseous-freelance <96338130+Eliseous-freelance@users.noreply.github.com>
2022-04-29 13:56:19 +0100

    tried to change bypass the dabase creation

--- assignment/website/application/controller/controller.php
+++ assignment/website/application/controller/controller.php
@@ -17,37 +17,12 @@ class Controller
 
         if ($pageID == 'home'){
             $this->model = new Model('sqlite:/its/home/ed385/public_html/3dapp/assignment/website/application/db/infoData.db');
-            $this->model->dbCreateDrinkDetailsTable();
 
-            $fantaText = "We're all born playful creatures. But the older we become, the less permission we give
-                      ourselves to be playful and we can easily feel burnt out or overwhelmed. All this seriousness, we call it
-                      the 'Grey'.
-                      But we can always fight back the 'Grey'. How? By taking inspiration from the ones who stayed spontaneous
-                      and playful. The ones who bring colour to the world every day. The Colourful People.
-                      With our new campaign, Fanta wants to celebrate these Colourful People and reignite that spirit into
-                      people from all ages. So grab a Fanta and come play colourful with us.";
-            $pepperText = "Dr Pepper's unique, sparkling blend of 23 fruit flavours has been around for well over
-              a century amd it's still the same, with that distinctive flavour you just can't quite put your toungue on.
-              It was created by Texas pharmacist Charles Alderton in 1885. Hw gave a sample of the first ever batch to
-              Wade Morrison, a local shop owner, and Mr Morrison instantly agreed to stock the drink.
-                The distinctive, bold taste of Dr Pepper has been popular ever since.";
-            $cokeText = "In 1886 in New York Harbour, workers were constructing the Statue of Liberty. Eight
-              hundred miles away, another great American symbol was about to be unveiled. Like many people who change
-              history, John Stith Pemberton, an Atlanta pharmacist, was inspired by simple curiosity. One afternoon, he
-              stirred up a fragrant, caramel-coloured liquid and, when it was done ... booyah! He had invented Coca
-              Cola.";
-            $spriteText = "First introduced in 1961, crisp, refreshing, clean-tasting Sprite is now the world's
-              leading lemon and lime flavoured soft drink and is sold in more than 190 different countries. Sprite Zero,
-              part of Coca Cola's no sugar Zero range, offers the delicious lemon lime taste of Sprite without the sugar
-              or calories.";
             $pepperPath = "/its/home/ed385/public_html/3dapp/assignment/website/assets/images/homepage/dr_pepper.jpg";
             $fantaPath = "/its/home/ed385/public_html/3dapp/assignment/website/assets/images/homepage/fanta.png";
             $cokePath = "/its/home/ed385/public_html/3dapp/assignment/website/assets/images/homepage/coca_cola.jpg";
             $spritePath = "/its/home/ed385/public_html/3dapp/assignment/website/assets/images/homepage/sprite.jpg";
-            $this->model->dbInsertDrinkDetails($title='Dr Pepper', $text=$pepperText, $imagePath=$pepperPath);
-            $this->model->dbInsertDrinkDetails($title='Coca Cola', $text=$cokeText, $cokePath);
-            $this->model->dbInsertDrinkDetails($title='Sprite', $text=$spriteText, $spritePath);
-            $this->model->dbInsertDrinkDetails($title='Fanta', $text=$fantaText, $fantaPath);
+
             $pepperDescriptionData = $this->apiGetInfo($model_function=$this->model->dbGetDrinkDetails());
             $fantaDescriptionData = $this->apiGetInfo($model_function=$this->model->dbGetDrinkDetails());
             $cokeDescriptionData = $this->apiGetInfo($model_function=$this->model->dbGetDrinkDetails());
@@ -60,32 +35,25 @@ class Controller
         else if ($pageID == 'coke'){
             $cokePath = "/its/home/ed385/public_html/3dapp/assignment/website/assets/images/homepage/coca_cola.jpg";
             $this->model = new Model('sqlite:/its/home/ed385/public_html/3dapp/assignment/website/application/db/modelData.db');
-            $this->model->dbCreateModelTable();
-            $this->model->dbInsertModelData($ID=1, $x3dCreationMethod='Blender', $modelTitle='Coca Cola', $modelSubtitle='', $modelDescription='$pepperModelDescription');
+
             $pepperModelData = $this->apiGetInfo($model_function=$this->model->dbGetModelData());
             $this->load->view($fileName=$cokePath, $data=$pepperModelData);
         }
         else if ($pageID == 'sprite'){
             $spritePath = "/its/home/ed385/public_html/3dapp/assignment/website/assets/images/homepage/sprite.jpg";
             $this->model = new Model('sqlite:/its/home/ed385/public_html/3dapp/assignment/website/application/db/modelData.db');
-            $this->model->dbCreateModelTable();
-            $this->model->dbInsertModelData($ID=2, $x3dCreationMethod='Blender', $modelTitle='Sprite', $modelSubtitle='', $modelDescription='$pepperModelDescription');
             $pepperModelData = $this->apiGetInfo($model_function=$this->model->dbGetModelData());
             $this->load->view($fileName=$spritePath, $data=$pepperModelData);
         }
         else if ($pageID == 'fanta'){
             $fantaPath = "/its/home/ed385/public_html/3dapp/assignment/website/assets/images/homepage/fanta.png";
             $this->model = new Model('sqlite:/its/home/ed385/public_html/3dapp/assignment/website/application/db/modelData.db');
-            $this->model->dbCreateModelTable();
-            $this->model->dbInsertModelData($ID=4, $x3dCreationMethod='Blender', $modelTitle='Fanta', $modelSubtitle='', $modelDescription='$pepperModelDescription');
             $pepperModelData = $this->apiGetInfo($model_function=$this->model->dbGetModelData());
             $this->load->view($fileName=$fantaPath, $data=$pepperModelData);
         }
         else if ($pageID == 'pepper'){
             $pepperPath = "/its/home/ed385/public_html/3dapp/assignment/website/assets/images/homepage/dr_pepper.jpg";
             $this->model = new Model('sqlite:/its/home/ed385/public_html/3dapp/assignment/website/application/db/modelData.db');
-            $this->model->dbCreateModelTable();
-            $this->model->dbInsertModelData($ID=3, $x3dCreationMethod='Blender', $modelTitle='Pepper', $modelSubtitle='', $modelDescription='$pepperModelDescription');
             $pepperModelData = $this->apiGetInfo($model_function=$this->model->dbGetModelData());
             $this->load->view($fileName=$pepperPath, $data=$pepperModelData);
         }
--- assignment/website/application/model/model.php
+++ assignment/website/application/model/model.php
@@ -11,14 +11,16 @@ class Model
 
     /**
      * constructor to create database connection using PHP Data Objects (PDO) as the interface to SQLite
+     * @param PDO $handle
      */
     public function __construct($dsn)
     {
         //Set up the database source name (DSN)
         $this->dsn = $dsn;
+        $this->handle = $this->connectDB($this->dsn);
+    }
 
-        //then create a connection to a database with the PDO() function 
-        //connect to the database table and retrieve the required brand data
+    public function connectDB($dsn){
         try {
             //Change connection string for different databases, currently using SQLite
             $this->handle = new PDO($dsn, 'user', 'password', array(
@@ -27,135 +29,34 @@ class Model
             ));
             $this->handle->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 
-            echo 'Database connection created';
+            echo "Database connection created \n";
+            return $this->handle;
         } catch (PDOEXception $e) {
-            echo "I can't connect to the database! D: ";
+            echo "I can't connect to the database! D:";
             print new Exception($e->getMessage());
         }
-    }
-
-    public function dbCreateModelTable()
-    {
-        try {
-            $this->handle->exec("
-                    CREATE TABLE  Model_3D (
-                        ID INTEGER PRIMARY KEY, 
-                        x3dCreationMethod TEXT NOT NULL, 
-                        modelTitle TEXT NOT NULL, 
-                        modelSubtitle TEXT NOT NULL, 
-                        modelDescription TEXT NOT NULL);");
-            return "Model_3D table is successfully created insIDe infoData.db file";
-        } catch (PDOException $e) {
-            print new Exception($e->getMessage());
-        }
-        $this->handle = NULL;
-    }
-
-//    public function dbCreateDrinkDetailsTable()
-//    {
-//        try {
-//            $this->handle->exec("
-//                    CREATE TABLE  DrinkDetails (
-//                        ID INTEGER PRIMARY KEY,
-//                        title VARCHAR(1000) NOT NULL,
-//                        text TEXT NOT NULL,
-//                        imagePath TEXT NOT NULL);");
-//            return "drink details table is successfully created insIDe infoData.db file";
-//        } catch (PDOException $e) {
-//            print new Exception($e->getMessage());
-//        }
-//        $this->handle = NULL;
-//    }
-
-    public function dbInsertModelData($ID, $x3dCreationMethod, $modelTitle, $modelSubtitle, $modelDescription)
-    {
-        echo "Data insertion function";
-        try {
-            $this->handle->exec("INSERT INTO Model_3D (ID, x3dModelTitle, x3dCreationMethod, modelTitle, modelSubstitle, modelSubtitle)
-                                    SELECT ($x3dCreationMethod, $modelTitle, $modelSubtitle, $modelDescription)
-                                    VALUES ($ID, $x3dCreationMethod, $modelTitle, $modelSubtitle, $modelDescription)
-                                    WHERE NOT EXISTS(SELECT $ID FROM Model_3D WHERE ID = $ID AND x3dCreationMethod = $x3dCreationMethod AND modelTitle = $modelTitle AND modelSubtitle = $modelSubtitle AND modelDescription = $modelSubtitle)");
-
-
-
-            return "X3D model data is inserted successfully insIDe modelData.db";
-        } catch (PDOException $e) {
-            print new Exception($e->getMessage());
-        }
-        $this->handle = NULL;
-    }
-
-    public function dbInsertDrinkDetails($title, $text, $imagePath)
-    {
-        echo "Data insertion function";
-        try {
-            $this->handle->exec("INSERT INTO DrinkDetails (title) VALUES($title)"); //, description, imagePath , $text, $imagePath
-//            $this->handle->exec("INSERT INTO DrinkDetails (ID, title, description, imagePath)
-//                                    SELECT ($title, $text, $imagePath),
-//                                    VALUES ($ID, $title, $text, $imagePath),
-//                                    WHERE NOT EXISTS(SELECT $ID FROM drinkDetails WHERE ID = $ID AND title = $title AND description = $text AND imagePath = $imagePath);");
-            return "X3D model data is inserted successfully insIDe test1.db";
-        } catch (PDOException $e) {
-            print new Exception($e->getMessage());
-        }
-        $this->handle = NULL;
-    }
-    
-    public function dbGetModelData()
-    {
-        echo "Data retrieval function";
-        try {
-            //prepare a statement to get all records from the Model_3D table
-            $sql = 'SELECT * FROM Model_3D';
-            //use PDO query() to query the database with the prepared SQL statement
-            $stmt = $this->handle->query($sql);
-
-            //prepare a SQL statement to select a record matching the brand name selected in the view drop-down list
-
-            //user up an array to return the results to the view
-            $result = null;
-            //set up a variable to index each row of the array
-            $i = -0;
-            //use PDO fetch() to retrieve the results from the database using a while loop
-            //use a while loop to loop through the rows
-            while ($data = $stmt->fetch()) {
-                $result[$i]['x3dModelData'] = $data['x3dModelData'];
-                $result[$i]['modelTitle'] = $data['modelTitle'];
-                $result[$i]['modelSubtitle'] = $data['modelSubtitle'];
-                $result[$i]['modelDescription'] = $data['modelDescription'];
-                //increment the row index
-                $i++;
-            }
-        } catch (PDOException $e) {
-            print new Exception($e->getMessage());
-            echo json_encode($result);
-        }
-        //close the database connection
-        $this->handle = NULL;
-        //send the response back to the view
-        return $result;
     }
 
     public function dbGetDrinkDetails(){
         {
-            echo "Data retrieval function";
             try {
+                echo "Data retrieval function \n";
                 //prepare a statement to get all records from the drinkDetails table
-                $sql = 'SELECT * FROM DrinkDetails';
+                $sql = "SELECT title FROM DrinkDetails";
+
                 //use PDO query() to query the database with the prepared SQL statement
                 $stmt = $this->handle->query($sql);
 
-                //prepare a SQL statement to select a record matching the brand name selected in the view drop-down list
-
                 //user up an array to return the results to the view
                 $result = null;
+
                 //set up a variable to index each row of the array
-                $i = -0;
+                $i = 0;
                 //use PDO fetch() to retrieve the results from the database using a while loop
                 //use a while loop to loop through the rows
                 while ($data = $stmt->fetch()) {
                     $result[$i]['title'] = $data['title'];
-                    $result[$i]['text'] = $data['text'];
+                    $result[$i]['description'] = $data['description'];
                     $result[$i]['imagePath'] = $data['imagePath'];
                     //increment the row index
                     $i++;
@@ -169,6 +70,38 @@ class Model
             //send the response back to the view
             return $result;
         }
+    }
+
+
+    public function dbGetModelData()
+    {
+        echo "Data retrieval function";
+        try {
+            //prepare a statement to get all records from the Model_3D table
+            $sql = 'SELECT * FROM Model_3D';
+            //use PDO query() to query the database with the prepared SQL statement
+            $stmt = $this->handle->query($sql);
+            $result = null;
+            //set up a variable to index each row of the array
+            $i = 0;
+            //use PDO fetch() to retrieve the results from the database using a while loop
+            //use a while loop to loop through the rows
+            while ($data = $stmt->fetch()) {
+                $result[$i]['title'] = $data['title'];
+                $result[$i]['method'] = $data['method'];
+                $result[$i]['subtitle'] = $data['subtitle'];
+                $result[$i]['description'] = $data['description'];
+                //increment the row index
+                $i++;
+            }
+        } catch (PDOException $e) {
+            print new Exception($e->getMessage());
+            echo json_encode($result);
+        }
+        //close the database connection
+        $this->handle = NULL;
+        //send the response back to the view
+        return $result; //you might want to jseon_encode
     }
 
 }
